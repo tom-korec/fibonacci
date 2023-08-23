162f11ea7d531b7d8d715c0cc02f5ee80bece1fe
Tomáš Korec <tom.korec7@gmail.com>
2023-08-23 22:46:20 +0200

    Result styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,16 @@ import { trpcClient } from "~/api/trpcClient";
 import { FibonacciForm } from "~/components/FibonacciForm";
 
 export default function Home() {
-  const [result, setResult] = useState<bigint | null>(null);
+  const [result, setResult] = useState<{
+    inputValue: number;
+    result: bigint;
+  } | null>(null);
 
   const handleSubmit = (n: number) => {
     trpcClient.fibonacci.compute
       .query({ n })
       .then((result) => {
-        setResult(result.result);
+        setResult({ inputValue: n, result: result.result });
       })
       .catch((error: { message?: string }) => {
         const parsedErrors = JSON.parse(error.message ?? "{}") as TRPCError[];
@@ -33,15 +36,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-slate-100 sm:text-[5rem]">
+      <main className="flex min-h-screen flex-col gap-[8rem] items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 mt-[10%] ">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight text-slate-100 sm:text-[5rem]">
             Fibonacci <span className="text-[hsl(280,100%,70%)]">Computer</span>
           </h1>
 
           <FibonacciForm handleSubmit={handleSubmit} />
-
-          {result !== null && <p>{result.toString()}</p>}
+        </div>
+        <div className={"container flex flex-col items-center"}>
+          <p
+            className={
+              "border-1 w-1/2 break-words rounded-md border-2 border-solid border-slate-100 px-12 py-6 text-center text-slate-100"
+            }
+            title={
+              result
+                ? `Fibonacci number for input value: ${result.inputValue}`
+                : undefined
+            }
+          >
+            {result ? result.result.toString() : <span className={"text-8xl"}>?</span>}
+          </p>
         </div>
       </main>
     </>
