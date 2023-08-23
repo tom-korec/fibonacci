7aa75c96da023f56f824802e43ad6946dc332695
Tomáš Korec <tom.korec7@gmail.com>
2023-08-23 23:05:26 +0200

    Fixed client api

--- src/api/trpcClient.ts
+++ src/api/trpcClient.ts
@@ -2,11 +2,18 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
 
 import type { AppRouter } from "~/server/api/root";
+import * as process from "process";
+
+const vercelUrl = process.env.VERCEL_URL;
+
+const url = vercelUrl
+  ? `https://${vercelUrl}/api/trpc`
+  : `http://localhost:3000/api/trpc`;
 
 export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/api/trpc",
+      url,
     }),
   ],
   transformer: superjson,
